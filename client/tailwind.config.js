d53ff7292e8e44345229f52cccb428e29440ed6e
dz0o0 <i.k.black619@icloud.com>
2024-05-24 00:27:37 +0900

    feat: Tailwind CSSのプラグインの追加と設定の変更

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
 }
 
