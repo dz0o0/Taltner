8eb7fa3440537d12b1d974aa340b3aa00e1c7389
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-05-28 17:26:35 +0900

    feat: .pyでモデルの推論ができるように変更

--- server/src/OpenVINO/llm_config.py
+++ server/src/OpenVINO/llm_config.py
@@ -7,14 +7,7 @@ DEFAULT_SYSTEM_PROMPT = """\
 質問の答えがわからない場合は、誤った情報を共有しないでください。\
 """
 
-
-def youri_partial_text_processor(partial_text: str, new_text: str) -> str:
-    new_text = new_text.replace("システム:", "")
-    partial_text += new_text
-    return partial_text
-
-
-LLM_MODELS_CONFIG = {
+LLM_MODELS_CONFIG: dict[str, dict[str, str | bool]] = {
     "gemma-2b-it": {
         "model_id": "google/gemma-2b-it",
         "remote_code": False,
@@ -22,7 +15,7 @@ LLM_MODELS_CONFIG = {
         "history_template": "<start_of_turn>user{user}<end_of_turn><start_of_turn>model{assistant}<end_of_turn>",
         "current_message_template": "<start_of_turn>user{user}<end_of_turn><start_of_turn>model{assistant}",
         "prompt_template": "<start_of_turn>user{user}<end_of_turn><start_of_turn>model",
-        "stop_tokens": ["<end_of_text>"],
+        "stop_tokens": "<end_of_text>",
     },
     "Phi-3-mini-4k-instruct": {
         "model_id": "microsoft/Phi-3-mini-4k-instruct",
@@ -31,16 +24,6 @@ LLM_MODELS_CONFIG = {
         "history_template": "<|user|>\n{user}<|end|> \n<|assistant|>\n{assistant}<|end|>\n",
         "current_message_template": "<|user|>\n{user}<|end|> \n<|assistant|>\n{assistant}",
         "prompt_template": "<|user|>\n{user}<|end|> \n<|assistant|>\n",
-        "stop_tokens": ["<|end|>"],
-    },
-    "youri-7b-chat": {
-        "model_id": "rinna/youri-7b-chat",
-        "remote_code": False,
-        "start_message": "設定: {SYSTEM_PROMPT}\n",
-        "history_template": "ユーザー: {user}\nシステム: {assistant}\n",
-        "current_message_template": "ユーザー: {user}\nシステム: {assistant}",
-        "prompt_template": "ユーザー: {user}\nシステム: ",
-        "tokenizer_kwargs": {"add_special_tokens": False},
-        "partial_text_processor": youri_partial_text_processor,
+        "stop_tokens": "<|end|>",
     },
 }
