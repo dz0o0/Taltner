452ffd61d6f850841c1633d919932bbc99bc4ad3
nka21 <naoki.nico@gmail.com>
2024-05-28 17:04:15 +0900

    feat: 画面全体にスライドの表示とカラー設定

---
+++ client/src/features/tutorial/components/Tutorial.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Tutorial() {
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-black opacity-70">
+      {/* スライド画面 */}
+      <div className="m-4 absolute inset-1 flex items-center justify-center bg-slate-300"></div>
+    </div>
+  );
+}
