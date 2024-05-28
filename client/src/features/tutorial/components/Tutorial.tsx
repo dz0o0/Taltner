49fa4ec763727d0c9982016350c4219db4bc7c92
nka21 <naoki.nico@gmail.com>
2024-05-28 17:30:00 +0900

    feat: スライドの外側をクリック時に、スライドが閉じるように実装
    handleOpen関数はチュートリアルを表示するボタンなどで後に使用する。

--- client/src/features/tutorial/components/Tutorial.tsx
+++ client/src/features/tutorial/components/Tutorial.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function Tutorial() {
+  // 状態変数とその更新関数を定義します。初期値はtrueです。
+  const [isOpen, setIsOpen] = useState(true);
+
+  // handleOpen関数は、isOpenの状態をtrueに設定します。
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  // handleClose関数は、isOpenの状態をfalseに設定します。
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className="absolute inset-0 flex items-center justify-center bg-black opacity-70">
-      {/* スライド画面 */}
-      <div className="m-4 absolute inset-1 flex items-center justify-center bg-slate-300"></div>
-    </div>
+    <>
+      {isOpen && (
+        <div
+          onClick={handleClose}
+          className="absolute inset-0 flex items-center justify-center bg-black opacity-70"
+        >
+          {/* スライド画面 */}
+          <div
+            onClick={(event) => event.stopPropagation()}
+            className="m-4 absolute inset-1 flex items-center justify-center bg-slate-300"
+          ></div>
+        </div>
+      )}
+    </>
   );
 }
