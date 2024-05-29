69a50216f455a6b88f20988a32a3ae1afe8468bd
dz0o0 <i.k.black619@icloud.com>
2024-05-29 21:49:29 +0900

    bugfix: 雑コミット

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,20 +1,11 @@
-import  ReceiveApi  from "../features/generateTopic/components/ReceiveApi";
-import AudioRecorder from "../features/generateTopic/components/AudioRecorder";
+"use client";
+
 import { Tutorial } from "@/features/tutorial/components/Tutorial";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <AudioRecorder />
-      </div>
-
-      <div>
-        <ReceiveApi />
-      </div>
-      <div className="">
+    <div className="">
       <Tutorial />
-      </div>
-    </main>
+    </div>
   );
 }
