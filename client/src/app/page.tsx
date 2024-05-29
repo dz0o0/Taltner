3a902a1647eff4697806a7f30d418d2dd13e5c33
dz0o0 <i.k.black619@icloud.com>
2024-05-30 00:36:32 +0900

    feat: 雑コミット

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { Tutorial } from "@/features/tutorial/components/Tutorial";
+import  ReceiveApi  from "../features/generateTopic/components/ReceiveApi";
+import AudioRecorder from "../features/generateTopic/components/AudioRecorder";
 
 export default function Home() {
   return (
     <div className="">
       <Tutorial />
+      <AudioRecorder />
+      <ReceiveApi />
     </div>
   );
 }
--- client/src/features/tutorial/components/Tutorial.tsx
+++ client/src/features/tutorial/components/Tutorial.tsx
@@ -24,7 +24,7 @@ export function Tutorial() {
         // 黒背景
         <div
           onClick={handleClose}
-          className="absolute inset-0 flex items-center justify-center overflow-hidden bg-black opacity-70"
+          className="absolute inset-0 flex items-center justify-center overflow-hidden bg-black bg-opacity-70"
         >
           {/* 黒背景の内側 */}
           <div
