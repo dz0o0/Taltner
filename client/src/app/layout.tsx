67c9b15708e6db37695f5a4fcea239501f9fa73c
nka21 <naoki.nico@gmail.com>
2024-05-30 19:17:57 +0900

    feat: メイン画面のスクロール無効化

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className="overflow-hidden min-h-screen">{children}</body>
+      <body className="min-h-screen overflow-hidden">{children}</body>
     </html>
   );
 }
