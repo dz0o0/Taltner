de1b4ca01f736a5df6a0f93bdda14d257fea652d
nka21 <naoki.nico@gmail.com>
2024-05-30 14:21:20 +0900

    chore: shadcn/ui add Accordion

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable sort-keys-custom-order/object-keys */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
@@ -8,6 +9,21 @@ module.exports = {
         background: "#f9fbfe", // カスタム背景色
         border: "#dee2e6", // カスタム境界線色
         foreground: "#333333",
+      },
+      // shadcn Acordionコンポーネントの設定
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
     },
   },
