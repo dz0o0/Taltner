9a15a0157a62f4005a31250b77a9b010287fda83
nka21 <naoki.nico@gmail.com>
2024-05-30 19:36:01 +0900

    fix: コンフリクトの修正

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,65 +1,16 @@
 "use client";
-import { useEffect } from "react";
 
-import { Button } from "@/components/ui/button";
-import { Sidebar } from "@/features/main/components/Accordion";
+import { Tutorial } from "@/features/tutorial/components/Tutorial";
+
+// import  ReceiveApi  from "../features/generateTopic/components/ReceiveApi";
+import AudioRecorder from "../features/generateTopic/components/AudioRecorder";
 
 export default function Home() {
-  useEffect(() => {
-    // ビューポートの高さを設定する関数
-    const setVh = () => {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-
-    // 初期のビューポートの高さを設定
-    setVh();
-
-    // ウィンドウのリサイズイベントに応じてビューポートの高さを更新
-    window.addEventListener("resize", setVh);
-
-    // コンポーネントがアンマウントされたときにイベントリスナーを削除
-    return () => {
-      window.removeEventListener("resize", setVh);
-    };
-  }, []);
-
   return (
-    <div className="flex h-screen w-full flex-row bg-progateBaseBG">
-      {/* <Tutorial /> */}
-      {/*
-        サイドバー
-        width chatGPTのサイドバーが２６０pxで指定していたためそこからの引用
-      */}
-      <aside className="h-screen w-[260px] overflow-y-scroll border-r-2 border-r-progateSidebarBorder bg-progateSidebarBG p-4 pt-32 text-black ">
-        <Sidebar />
-      </aside>
-
-      {/*
-        メイン画面
-      */}
-      <main className="w-full overflow-hidden">
-        <div className="mt-32 flex flex-col items-center justify-center">
-          <h1 className="text-5xl font-bold">Taltner</h1>
-          <p className="text-2xl">Let&apos; s have a conversation.</p>
-        </div>
-        <section className="mx-32 my-16 rounded-md bg-white shadow-progate">
-          <div className="grid size-full grid-cols-2 gap-24 p-24">
-            <Button variant="default" className="h-24 shadow-progate">
-              <p className="text-base">今日のご飯はなんですか</p>
-            </Button>
-            <Button variant="default" className="h-24 shadow-progate">
-              <p className="text-base">昨日何時に寝ましたか</p>
-            </Button>
-            <Button variant="default" className="h-24 shadow-progate">
-              <p className="text-base">明日の予定はなんですか</p>
-            </Button>
-            <Button variant="default" className="h-24 shadow-progate">
-              <p className="text-base">好きなアーティストは誰ですか</p>
-            </Button>
-          </div>
-        </section>
-      </main>
+    <div className="">
+      <Tutorial />
+      <AudioRecorder />
+      {/* <ReceiveApi /> */}
     </div>
   );
 }
--- client/src/features/tutorial/components/Tutorial.tsx
+++ client/src/features/tutorial/components/Tutorial.tsx
@@ -1,16 +1,69 @@
-"use client";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 
-import { Tutorial } from "@/features/tutorial/components/Tutorial";
+import Carousel from "@/features/tutorial/components/Carousel";
 
-// import  ReceiveApi  from "../features/generateTopic/components/ReceiveApi";
-import AudioRecorder from "../features/generateTopic/components/AudioRecorder";
+export function Tutorial() {
+  // 状態変数とその更新関数を定義します。初期値はtrueです。
+  const [isOpen, setIsOpen] = useState(true);
 
-export default function Home() {
+  // handleOpen関数は、isOpenの状態をtrueに設定します。
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  // handleClose関数は、isOpenの状態をfalseに設定します。
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className="">
-      <Tutorial />
-      <AudioRecorder />
-      {/* <ReceiveApi /> */}
-    </div>
+    <>
+      {isOpen && (
+        // 黒背景
+        <div
+          onClick={handleClose}
+          className="absolute inset-0 flex items-center justify-center overflow-hidden bg-black/70"
+        >
+          {/* 黒背景の内側 */}
+          <div
+            onClick={(event) => event.stopPropagation()}
+            className="absolute inset-4 flex bg-slate-100"
+          >
+            {/* スライド取り消しボタン */}
+            <button
+              onClick={handleClose}
+              className="absolute right-8 top-6 text-3xl transition-colors duration-200 hover:text-slate-500"
+            >
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            {/* スライドショー本体 */}
+            <Carousel>
+              {/* スライド１枚目 */}
+              <section>
+                <h1 className="text-5xl">Taltnerをはじめよう</h1>
+              </section>
+              {/* スライド2枚目 */}
+              <section>
+                <h1 className="text-5xl">ボタンをクリックで喋ってみよう</h1>
+              </section>
+              {/* スライド3枚目 */}
+              <section>
+                <h1 className="text-5xl">ボタンをクリックで送信してみよう</h1>
+              </section>
+              {/* スライド4枚目 */}
+              <section>
+                <h1 className="text-5xl">話題を選択してみよう</h1>
+              </section>
+              {/* スライド5枚目 */}
+              <section>
+                <h1 className="text-5xl">音声の入力は許可しているかな？</h1>
+              </section>
+            </Carousel>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
