df09219c005e5c83a2cbb9eda1b1b1885df1ca8d
nka21 <naoki.nico@gmail.com>
2024-05-30 19:19:24 +0900

    update: カラー設定

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,14 +9,54 @@ module.exports = {
         progateBaseBG: "#f9fbfe", // progate.comで使われているメイン背景色
         progateSidebarBG: "#ffffff", // progate.comで使われているサイドバーの背景色
         progateSidebarBorder: "#ececec", // progate.comで使われているサイドバーのborderRight色
+        background: "#f8f9fa", // カスタム背景色
         border: "#dee2e6", // カスタム境界線色
         foreground: "#333333",
+        // shadcn
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      // shadcn Buttonコンポーネントの設定
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        progate: "0 1px 6px rgba(0, 0, 0, 0.12), 0 1px 4px rgba(0, 0, 0, 0.12)",
       },
       // shadcn Acordionコンポーネントの設定
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -26,6 +66,10 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
