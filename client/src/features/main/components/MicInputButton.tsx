a14c449504753f0809cd6fabe7852a4995624c65
nka21 <naoki.nico@gmail.com>
2024-05-31 11:03:57 +0900

    feat: ボタンの切り替え機能の実装

--- client/src/features/main/components/MicInputButton.tsx
+++ client/src/features/main/components/MicInputButton.tsx
@@ -1,13 +1,32 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 export function MicInputButton() {
+  const [isStarted, setIsStarted] = useState(false);
+
+  const handleClick = () => {
+    setIsStarted(!isStarted);
+  };
+
   return (
     <div className="mb-8 mt-4 flex justify-center">
-      <Button variant="destructive" className="h-12 w-32 shadow-progate">
-        はじめる
-      </Button>
+      {isStarted ? (
+        <Button
+          variant="destructive"
+          className="h-12 w-32 shadow"
+          onClick={handleClick}
+        >
+          話題を生成する
+        </Button>
+      ) : (
+        <Button
+          variant="destructive"
+          className="h-12 w-32 shadow"
+          onClick={handleClick}
+        >
+          はじめる
+        </Button>
+      )}
     </div>
   );
 }
