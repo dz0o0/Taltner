f2b4e70410cb8d455aab64a1b011d9690d2924e4
nka21 <naoki.nico@gmail.com>
2024-05-31 09:38:01 +0900

    update: スタイルの調整

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,66 +1,51 @@
 "use client";
-import { useEffect } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Sidebar } from "@/features/main/components/Accordion";
 import { Tutorial } from "@/features/tutorial/components/Tutorial";
 
 export default function Home() {
-  useEffect(() => {
-    // ビューポートの高さを設定する関数
-    const setVh = () => {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-
-    // 初期のビューポートの高さを設定
-    setVh();
-
-    // ウィンドウのリサイズイベントに応じてビューポートの高さを更新
-    window.addEventListener("resize", setVh);
-
-    // コンポーネントがアンマウントされたときにイベントリスナーを削除
-    return () => {
-      window.removeEventListener("resize", setVh);
-    };
-  }, []);
-
   return (
-    <div className="flex h-screen w-full flex-row bg-progateBaseBG">
-      <Tutorial />
-      {/*
-        サイドバー
-        width chatGPTのサイドバーが２６０pxで指定していたためそこからの引用
-      */}
-      <aside className="h-screen w-[260px] overflow-y-scroll border-r-2 border-r-progateSidebarBorder bg-progateSidebarBG p-4 pt-32 text-black ">
-        <Sidebar />
-      </aside>
-
-      {/*
-        メイン画面
-      */}
-      <main className="w-full overflow-hidden">
-        <div className="mt-32 flex flex-col items-center justify-center">
-          <h1 className="text-5xl font-noto">Taltner</h1>
-          <p className="text-2xl">Let&apos; s have a conversation.</p>
-        </div>
-        <section className="mx-32 my-16 rounded-md bg-white shadow-progate">
-          <div className="grid size-full grid-cols-2 gap-24 p-24">
-            <Button variant="default" className="h-24 shadow-progate">
-              <p className="text-base">今日のご飯はなんですか</p>
-            </Button>
-            <Button variant="default" className="h-24 shadow-progate">
-              <p className="text-base">昨日何時に寝ましたか</p>
-            </Button>
-            <Button variant="default" className="h-24 shadow-progate">
-              <p className="text-base">明日の予定はなんですか</p>
-            </Button>
-            <Button variant="default" className="h-24 shadow-progate">
-              <p className="text-base">好きなアーティストは誰ですか</p>
-            </Button>
+    <>
+      <div className="flex h-screen w-full flex-row overflow-hidden">
+        <Tutorial />
+        <div className="shrink-0 overflow-x-hidden bg-progateSidebarBG">
+          <div className="h-full w-[260px]">
+            <aside className="size-full flex-1 overflow-auto border-r-2 border-r-progateSidebarBorder bg-progateSidebarBG p-4">
+              <Sidebar />
+            </aside>
           </div>
-        </section>
-      </main>
-    </div>
+        </div>
+        <div className="flex h-full max-w-full flex-1 flex-col overflow-hidden">
+          <main className="size-full flex-1 overflow-auto bg-progateBaseBG">
+            {/* アプリ名とキャッチコピー */}
+            <div className="mt-24 flex flex-col items-center justify-center">
+              <h1 className="font-capriola text-5xl">Taltner</h1>
+              <p className="font-noto  text-2xl">
+                Let&apos; s have a conversation.
+              </p>
+            </div>
+            <section className="m-32 flex rounded-md bg-progateSidebarBG  shadow-progate">
+              <div className="grid size-full grid-cols-2 gap-16 p-24">
+                <Button variant="default" className="h-24 shadow-progate">
+                  <p className="font-noto text-base">今日のご飯はなんですか</p>
+                </Button>
+                <Button variant="default" className="h-24 shadow-progate">
+                  <p className="font-noto text-base">昨日何時に寝ましたか</p>
+                </Button>
+                <Button variant="default" className="h-24 shadow-progate">
+                  <p className="font-noto text-base">明日の予定はなんですか</p>
+                </Button>
+                <Button variant="default" className="h-24 shadow-progate">
+                  <p className="font-noto text-base">
+                    好きなアーティストは誰ですか
+                  </p>
+                </Button>
+              </div>
+            </section>
+          </main>
+        </div>
+      </div>
+    </>
   );
 }
